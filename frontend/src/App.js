33962af68660eb692450dc7976edec9a36a0f52a
AnikSinghal <aniksinghal2104@gmail.com>
2025-03-21 16:53:33 +0530

    Fix: Routes Readded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Form from "./form.jsx";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginForm from "./loginForm.jsx";
+import RegForm from "./regForm.jsx";
 function App() {
   return (
-    <div className="App">
-      <Form/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Form />} />
+        {/* Below are example if you want to checck the working */}
+        {/* <Route path="/login" element={<LoginForm />} /> */}
+        {/* <Route path="/register" element={<RegForm/>} /> */}
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
