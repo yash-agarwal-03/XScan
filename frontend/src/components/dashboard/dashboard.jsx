084f78e52f59330e7dbc03b9cb191a93545d9bb0
Yash Agarwal <136333423+yash-agarwal-03@users.noreply.github.com>
2025-06-12 15:41:42 +0530

    Navbar functional.
    * basic navbar setup
    
    * Navbar with logout created
    
    * updated gitignore
    
    * Navbar basics complete
    
    * Removed the pyc files from navbar
    
    * Updated gitignore in navbar

--- frontend/src/components/dashboard/dashboard.jsx
+++ frontend/src/components/dashboard/dashboard.jsx
@@ -7,59 +7,47 @@ import ReportCard from "./reportCard";
 import ImageList from "./imageList";
 import { Link } from "react-router-dom";
 const Dashboard = () => {
-  const [modalOpen, setModalOpen] = useState(false);
-  const navigate = useNavigate();
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const storedUser = localStorage.getItem("user");
-    if (!storedUser) {
-      navigate("/"); // Redirect if not logged in
-    } else {
-      setUser(JSON.parse(storedUser));
+    const [modalOpen, setModalOpen] = useState(false);
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const handleLogout = () => {
+        localStorage.removeItem("user");
+        navigate("/"); // Redirect to login page
     }
-  }, [navigate]);
-  return (
-    <div>
-      {/* NAVBAR */}
-      <Navbar />
-      <h1 className="ms-4">Welcome, {user?.name}</h1>
-      <div
-        className="mt-5 text-center upload-section"
-        style={{ height: "15rem", width: "96.5vw", borderRadius: "2rem" }}
-      >
-        <div
-          className="d-flex justify-content-between align-items-center"
-          style={{ width: "85vw" }}
-        >
-          <button
-            className="upload-btn"
-            size="lg"
-            onClick={() => setModalOpen(true)}
-          >
-            Upload X-Ray
-          </button>
-          <img
-            src="/images/dashboard.png"
-            alt="Dashboard Icon"
-            className="img-fluid"
-            style={{ height: "16rem" }}
-          />
-        </div>
-        <UploadModal
-          isOpen={modalOpen}
-          toggle={() => setModalOpen(!modalOpen)}
-        />
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem("user");
+        if (!storedUser) {
+            navigate("/"); // Redirect if not logged in
+        } else {
+            setUser(JSON.parse(storedUser));
+        }
+    }, []);
+
+    return (
+        <div>
+            {/* NAVBAR */}
+            {user ? (
+                <Navbar user={user} onLogout={handleLogout} />) :
+                (
+                <Navbar user={null} onLogout={handleLogout} />) }
+
+            <div className="mt-5 text-center upload-section" style={{ height: "15rem", width: "96.5vw", borderRadius: "2rem" }}>
+                <div className="d-flex justify-content-between align-items-center" style={{ width: "85vw" }}>
+                    <button className="upload-btn" size="lg" onClick={() => setModalOpen(true)}>Upload X-Ray</button>
+                    <img src="/images/dashboard.png" alt="Dashboard Icon" className="img-fluid" style={{ height: "16rem" }} />
+                </div>
+                <UploadModal isOpen={modalOpen} toggle={() => setModalOpen(!modalOpen)} />
+            </div>
+            {/* REPORT CARD Mapping Loop */}
+            <h1 className="ms-4 mb-5">Reports</h1>
+            <ReportCard />
+            <Link to="/imageList">
+              <button className="ms-4 mb-5 p-1" style={{ width: "10rem", backgroundColor: "white", color: "black", borderRadius: "1rem" }}>
+                View Image List
+              </button>
+            </Link>
       </div>
-      {/* REPORT CARD Mapping Loop */}
-      <h1 className="ms-4 mb-5">Reports</h1>
-      <ReportCard />
-      <Link to="/imageList">
-        <button className="ms-4 mb-5 p-1" style={{ width: "10rem", backgroundColor: "white", color: "black", borderRadius: "1rem" }}>
-          View Image List
-        </button>
-      </Link>
-    </div>
   );
 };
 
--- frontend/src/components/dashboard/navbar.jsx
+++ frontend/src/components/dashboard/navbar.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Navbar = () => {
-    return (
-        <div>
-            
+const Navbar = ({ user, onLogout }) => {
+  return (
+    <div className="nav-container">
+      <div className="nav-box">
+        <img src="./images/logo.png" alt="logo" />
+        <h1>X-SCAN</h1>
+      </div>
+
+      <div className="profile-container">
+        <div className="nav-box">
+          <h2>{user?.name||"No user details"} </h2>  
+          <img
+            src="./images/profile.png"
+            alt="profile"
+            className="profile-icon"
+          />
         </div>
-    )
-}
+          <button className="logout-btn" onClick={onLogout}>
+            Logout
+          </button>
+        </div>
+      </div>
+  );
+};
+
 export default Navbar;
